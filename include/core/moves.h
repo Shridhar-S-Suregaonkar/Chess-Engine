f0cd69988ee774110f4c3921f83479933977e7ca
Shridhar-S-Suregaonkar <shridharsuregaonkar@gmail.com>
2025-10-17 23:14:00 +0530

    Enums value assignment

--- include/core/moves.h
+++ include/core/moves.h
@@ -6,12 +6,12 @@
 
 namespace board {
 
-    enum PieceType {
-        Pawn, Knight, Bishop, Rook, Queen, King
+    enum PieceType: uint8_t {
+        Pawn = 0, Knight = 1, Bishop = 2, Rook = 3, Queen = 4, King = 5
     };
 
-    enum Color {
-        White, Black
+    enum Color: uint8_t {
+        White = 0, Black = 1
     };
 
     const std::array<std::pair<uint8_t, uint8_t>, 8> knightMoves = {std::make_pair(2, 1), std::make_pair(1, 2), std::make_pair(-1, 2), std::make_pair(-2, 1), std::make_pair(-2, -1), std::make_pair(-1, -2), std::make_pair(1, -2), std::make_pair(2, -1)};
