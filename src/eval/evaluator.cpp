e9eb7291e8a73ce8c96d2e86b2b4550a00f5d9ff
Shridhar-S-Suregaonkar <shridharsuregaonkar@gmail.com>
2025-11-12 23:19:13 +0530

    Evaluator init

---
+++ src/eval/evaluator.cpp
@@ -0,0 +1,13 @@
+
+#include "../include/eval/evaluator.h"
+#include "../include/core/header.h"
+#include "../include/core/moves.h"
+#include "../include/core/board.h"
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+namespace eval {
+
+}
