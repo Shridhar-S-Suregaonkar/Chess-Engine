cd3a384e079b2d6053004ce967c58f69ee26bfdc
Shridhar-S-Suregaonkar <shridharsuregaonkar@gmail.com>
2025-11-12 23:20:07 +0530

    Search tree

---
+++ src/eval/search.cpp
@@ -0,0 +1,12 @@
+#include "../include/eval/evaluator.h"
+#include "../include/core/header.h"
+#include "../include/core/moves.h"
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+namespace eval {
+
+
+}
